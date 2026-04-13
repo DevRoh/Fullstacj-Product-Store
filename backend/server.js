a0449b8bf8d6535061959e7f29d9c388ae7818b3
Rohit Paul <paulrohit85@gmail.com>
2026-04-13 21:05:44 +0530

    feat: add get products endpoint

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ dotenv.config()
 const app = express()
 app.use(express.json()) // Allows us to accept JSON data in the body
 
+app.get('/api/products',async(req,res)=>{
+    try {
+        const products = await Product.find({}) // We give empty object here so that all the product find is given
+        res.status(200).json({success:true,products})
+    } catch (error) {
+        console.log("Error in fetching Products: ",error.message)
+        res.status(400).json({success:false,message:"Error in fetching Products"})
+    }
+})
+
 app.post('/api/products',async(req,res)=>{
     const product = req.body
 
