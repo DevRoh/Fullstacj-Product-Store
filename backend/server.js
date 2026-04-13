82f72ed5687b23f00bc9cf08e51fdbb342b0bb65
Rohit Paul <paulrohit85@gmail.com>
2026-04-13 21:00:21 +0530

    feat: add DELETE product route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import Product from './models/product.model.js'
 dotenv.config()
 
 const app = express()
+app.use(express.json()) // Allows us to accept JSON data in the body
 
 app.post('/api/products',async(req,res)=>{
     const product = req.body
@@ -21,8 +22,19 @@ app.post('/api/products',async(req,res)=>{
         res.status(201).json({success:true,data:newProduct})
 
     } catch (error) {
-        console.error(`Error in Create Product: ${error.message}`)
+        console.error("Error in Create Product: ",error.message)
         res.status(500).json({success:false,message:"Server Error"})
+    }
+})
+
+app.delete('/api/products/d/:id',async(req,res)=>{
+    const {id} = req.params
+    // console.log(id)
+    try {
+        await Product.findByIdAndDelete(id)
+        res.status(200).json({success: true,message: "Product deleted"})
+    } catch (error) {
+        res.status(404).json({success:false,message:"product not found"})
     }
 })
 
