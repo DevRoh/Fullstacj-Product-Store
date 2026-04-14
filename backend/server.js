4e591408318135e50a594c1d08d39c550a36ff7d
Rohit Paul <paulrohit85@gmail.com>
2026-04-14 17:46:37 +0530

    feat: deploy frontend with backend server

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
-import mongoose from "mongoose";
 import productRoute from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // Allows us to accept JSON data in the body
 
+const __dirname = path.resolve();
+
 app.use("/api/products", productRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 
 app.listen(process.env.PORT, () => {
   connectDB();
